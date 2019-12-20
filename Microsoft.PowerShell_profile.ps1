b9aa27e6e502925474bdb962a3acbb58a10bb5af
Kris <winframe@outlook.com>
2019-12-20 00:38:41 -0500

    alias for python2 (rip 2020) and add $pwd$ to prompt

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,6 +1,9 @@
 clear-host
 
 set-alias java8 'C:\Program Files\Java\jre1.8.0_231\bin\java.exe'
+set-alias python2 'C:\Program Files\Python27\python.exe'
+
 function Prompt {
-    "[pwsh] > "
+
+    "[pwsh] $pwd > "
 }
