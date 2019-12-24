3533a48ef7799bbbedd65651e40699764477f4d7
Kris <winframe@outlook.com>
2019-12-24 01:05:49 -0500

    remove aliases to outdated language versions, add computer name and user name to prompt

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,6 @@
 clear-host
 
-set-alias java8 'C:\Program Files\Java\jre1.8.0_231\bin\java.exe'
-set-alias python2 'C:\Program Files\Python27\python.exe'
-
 function Prompt {
 
-    "[pwsh] $pwd > "
+    "[pwsh] $env:username on $env:computername in $pwd `n> "
 }
