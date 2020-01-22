6875dc49c8d19a20b44bbc25357a5b62f5209260
Kris Jones <winframe@outlook.com>
2020-01-21 22:45:11 -0500

    function to check administrator status (powershell likes to reset its shortcuts to not always run as admin so i have to check sometimes)

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -4,3 +4,9 @@ function Prompt {
 
     "[pwsh] $env:username on $env:computername in $pwd `n> "
 }
+
+function Test-Administrator  
+{  
+    $user = [Security.Principal.WindowsIdentity]::GetCurrent();
+    (New-Object Security.Principal.WindowsPrincipal $user).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)  
+}
