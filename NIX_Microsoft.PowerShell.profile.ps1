320d0030006806b98bbec40e926692074f55cf16
Kris Jones <winframe@outlook.com>
2020-07-15 20:03:03 -0400

    Update NIX_Microsoft.PowerShell.profile.ps1

--- NIX_Microsoft.PowerShell.profile.ps1
+++ NIX_Microsoft.PowerShell.profile.ps1
@@ -7,9 +7,10 @@ $H/App - personal binaries
 $H/.cabal - Haskell Cabal
 $H/.cargo - Rust Cargo
 /opt/mss-t - MS-SQL CLI Tools
+/opt/ssis - SQL Server Integration Services
 /opt/vmw-vddk - VMware Virtual Disk Development Kit binaries #>
 
-$Env:PATH += ":$HOME/Applications/bin:$HOME/.cabal/bin:$HOME/.cargo/bin:/opt/mssql-tools/bin:/opt/vmware-vddk/bin64"
+$Env:PATH += ":$HOME/Applications/bin:$HOME/.cabal/bin:$HOME/.cargo/bin:/opt/mssql-tools/bin:/opt/ssis/bin:/opt/vmware-vddk/bin64"
 
 <# set aliases;
 java11 - JDK v11, AdoptOpenJDK OpenJ9 distribution
